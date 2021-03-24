dcfc4e75dcbdb8242f70ec2eb0ac16419f7ab336
masaxsuzu <masaxsuzu+github@gmail.com>
2021-03-24 20:45:43 +0900

    Add Top Level Statement

--- console/net50/Program.cs
+++ console/net50/Program.cs
@@ -1,28 +1,21 @@
 ﻿using System;
 using System.Linq;
 using v90.Features;
-namespace net50
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var features = new Func<string>[] {
-                () => new RecordType("Record type", 1).ToString(),
-                () => {
-                    var x = new InitOnlySetter() {
-                        X = "Init only setter",
-                        Y = 2,
-                    };
-                    // x.X = "Never override once initialized";
-                    return x.ToString();
-                },
-            };
 
-            foreach (var feature in features)
-            {
-                Console.WriteLine(feature());
-            }
-        }
-    }
+var features = new Func<string>[] {
+    () => new RecordType("Record type", 1).ToString(),
+    () => {
+        var x = new InitOnlySetter() {
+            X = "Init only setter",
+            Y = 2,
+        };
+        // x.X = "Never override once initialized";
+        return x.ToString();
+    },
+    () => new TopLevelStatement().ToString(),
+};
+
+foreach (var feature in features)
+{
+    Console.WriteLine(feature());
 }
---
+++ v90/Features/TopLevelStatement.cs
@@ -0,0 +1,9 @@
+namespace v90.Features
+{
+    public class TopLevelStatement {
+        public override string ToString()
+        {
+            return "Top level statement is added: System.Console.WriteLine(\"Hello World!\") is valid as Program.cs";
+        }
+    }
+}
